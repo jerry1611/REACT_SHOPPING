d546496314c8796db12a26d1453697a3b4bf0bf4
jerry1611 <pallisreeteja16@gmail.com>
2018-09-01 15:48:56 -0500

    idk

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            <button>Sumit</button>
+            <button>Submit</button>
          </div>
       );
    }
